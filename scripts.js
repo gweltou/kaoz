932f4fb9df57422b43243675a25b22d88c6201b0
gweltou <gweltou@hotmail.com>
2020-01-02 12:11:38 +0100

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,147 @@
+let date = new Date(0);
+let ajax = new XMLHttpRequest();
+let update_interval = 2000;
+let timer;
+ajax.responseType = 'document';
+
+
+ajax.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        /* KEMENNADENNOÙ RESEVET ! */
+        /*console.log(this.responseText);*/
+        
+        if (this.responseXML) {
+            messages = this.responseXML.getElementsByTagName("message");
+            for (let i = messages.length-1; i >= 0; i--) {
+                /* childrens: id, type, message-container */
+                let new_message = messages[i].children[2];
+                appendMessage(new_message, messages[i].children[1].textContent);
+                document.cookie = "last_id=" + messages[i].children[0].textContent;
+            }
+            if (messages.length > 0) {
+                /* Peek more frequently */
+                update_interval = Math.max(update_interval * 0.66, 200);
+                w3CodeColor();    /* Code color highlighting */
+            }
+        }
+    }
+};
+
+
+function appendMessage(message, content_type) {
+    console.log(content_type);
+    let frag = document.createRange().createContextualFragment(message.outerHTML);
+    let messages = document.getElementById("kemennadennou");
+    messages.insertBefore(frag, messages.childNodes[0]);
+}
+
+
+function updateKemennadennou(query_string) {
+    ajax.open("GET", "kemennadennou" + query_string, true);
+    ajax.send();
+        
+    /* Stretch update time interval */
+    timer = window.setTimeout(updateKemennadennou, update_interval, "");
+    update_interval = Math.floor(update_interval * 1.05);
+}
+
+
+function sendKemennadenn() {
+        let pseudo = document.getElementById("pseudo-input").value
+        /*date.setTime(Date.now() + 5*60*1000);*/
+        /*let expires = "expires="+ date.toUTCString();*/
+        document.cookie = "pseudo=" + encodeURIComponent(pseudo) /*+ ";" + expires;*/
+        let query_string = "?p=";
+        query_string += encodeURIComponent(pseudo);
+        query_string += "&k=";
+        query_string += encodeURIComponent(document.getElementById("kemennadenn-input").value);
+        
+        clearTimeout(timer);
+        update_interval = 2000;
+        updateKemennadennou(query_string);
+        document.getElementById("kemennadenn-input").select();
+      }
+      
+      function checkCookiesEnabled() {
+        if (navigator.cookieEnabled == false) {
+          window.alert("Cookies are not enabled");
+        }
+}
+
+
+function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i = 0; i < ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+          }
+    }
+    
+    return "";
+}
+
+
+function pageInit() {
+    checkCookiesEnabled();
+    if (getCookie("pseudo") == "") {
+            document.getElementById('karta').style.display='block';
+            document.getElementById("pseudo-input").value = "pseudo" + Math.floor(Math.random()*1000).toString();
+    } else {
+        document.getElementById("pseudo-input").value = getCookie("pseudo");
+    }
+        
+    document.cookie = "last_id=none"
+    update_interval = 2000; /* UGLY */
+    updateKemennadennou("");
+}
+      
+      /*
+      String.prototype.hashCode = function() {
+        var hash = 0, i, chr;
+        if (this.length === 0) return hash;
+        for (i = 0; i < this.length; i++) {
+          chr   = this.charCodeAt(i);
+          hash  = ((hash << 5) - hash) + chr;
+          hash |= 0; // Convert to 32bit integer
+        }
+        return hash;
+      };*/
+
+
+/* HTML Formatter
+   https://stackoverflow.com/questions/26360414/javascript-how-to-correct-indentation-in-html-string
+
+*/
+/*
+function process(str) {
+    var div = document.createElement('div');
+    div.innerHTML = str.trim();
+
+    return format(div, 0).innerHTML;
+}
+*/
+
+function format(node, level) {
+    var indentBefore = new Array(level++ + 1).join('  '),
+        indentAfter  = new Array(level - 1).join('  '),
+        textNode;
+
+    for (var i = 0; i < node.children.length; i++) {
+        textNode = document.createTextNode('\n' + indentBefore);
+        node.insertBefore(textNode, node.children[i]);
+
+        format(node.children[i], level);
+
+        if (node.lastElementChild == node.children[i]) {
+            textNode = document.createTextNode('\n' + indentAfter);
+            node.appendChild(textNode);
+        }
+    }
+
+    return node;
+}
---
+++ servijer.py
@@ -0,0 +1,154 @@
+# coding: utf-8
+
+import os.path
+from http.server import BaseHTTPRequestHandler
+from http.cookies import SimpleCookie
+import html
+import socketserver
+from urllib.parse import parse_qs
+from io import BytesIO
+import sys
+from datetime import datetime
+from uuid import uuid1
+
+
+MESSAGE_INTERVAL = 4    # E eilenn
+LOG_SIZE = 50           # Niver a c'hemennadenn dre fichennaoueg
+
+
+KEMENNADENN = """
+  <message>
+    <id>{}</id>
+    <type>{}</type>
+    <div class="message-container" style="background-color:hsl({}, 60%, 96%);">
+      <div class="pseudo-container">
+        <div class="pseudo" style="color:hsl({}, 68%, 60%);background-color:hsl({}, 60%, 88%);">{}</div>
+      </div>
+      <div class="message-body">
+        <div class="message-plain">{}</div>
+      </div>
+    </div>
+  </message>"""
+
+
+XML_HEADER = '<?xml version="1.0" encoding="UTF-8"?>\n'
+XML_ROOT_START = "<messages>"
+XML_ROOT_END = "\n</messages>"
+
+class MyHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        host = self.client_address[0] + ':' + str(self.client_address[1])
+    
+        # Lenn ar cookie
+        cookies = SimpleCookie(self.headers.get('Cookie'))
+        print(cookies)
+        if "last_id" in cookies:
+            last_id = cookies["last_id"].value
+        else:
+            last_id = ""
+        
+        query_dict = {}
+        if self.path.find('?') > 0:
+            path, query = self.path.split("?")
+            query_dict = parse_qs(query)
+            print(query_dict)
+            if "p" in query_dict and "k" in query_dict:
+                # Kemennadenn resevet
+                pseudo = query_dict["p"][0][:32]   # 32 chars max
+                pseudo_lower = pseudo.lower()
+                message = query_dict["k"][0]
+                if host not in users or users[host]["pseudo"] != pseudo:
+                    # Deraouiñ an implijer nevez ha dibab ul liv evitañ
+                    b = hash(pseudo_lower).to_bytes(8, byteorder='big', signed='signed')
+                    s = sum(b)
+                    hue = s%360
+                    users[host] = {
+                                    "pseudo": pseudo,
+                                    "hue": hue,
+                                    "last_message": datetime.min,
+                                    #"num_messages": 0,
+                                  }
+                    
+                # Check the client activity interval,
+                # refuse client if too short
+                now = datetime.now()
+                if (now-users[host]["last_message"]).total_seconds() > MESSAGE_INTERVAL:
+                    # Asantiñ ar c'hemennadenn hag enrollañ anezhi
+                    users[host]["last_message"] = now
+                    #users[host]["num_messages"] += 1
+                    print("RECORD " + message)
+                    # ID, IP, deiziat, type, pseudo, kemennadenn
+                    content_type = "text"
+                    if ('<' in message) and ('>' in message):
+                        content_type = "html"
+                    messages.insert(0,
+                        (str(uuid1()), "", now.isoformat(), content_type, pseudo, message))
+        
+        else:
+            path = self.path
+        
+        self.send_response(200)
+        self.end_headers()
+        
+        if path == "/clear":
+            messages.clear()
+            self.wfile.write(pajenn_degemer)
+        
+        elif path == '/' or path == "/index.html":
+            self.wfile.write(pajenn_degemer)
+        
+        elif path == "/kemennadennou":
+            # Goulenn AJAX eus ar c'hliant
+            # Adkas ar c'hemennadennoù d'ar c'hliant
+            xmldoc = ""
+            for m in messages:
+                id = m[0]
+                if id == last_id:
+                    # Don't send older messages than last_id
+                    break
+                    
+                content_type = m[3]
+                pseudo = m[4]
+                message = m[5]
+                hue = users[host]["hue"]   
+                xmldoc += KEMENNADENN.format(
+                    id,
+                    content_type,
+                    hue,
+                    hue,
+                    (hue+180)%360,
+                    pseudo,
+                    message)
+            
+            if xmldoc:
+                response = ""
+                response += XML_HEADER
+                response += XML_ROOT_START
+                response += xmldoc
+                response += XML_ROOT_END
+                #print("response:", response)
+                self.wfile.write(response.encode("utf-8")) ### Kollet eurvezhioù debugiñ evit ur gudenn a indentation amañ :(
+            
+        else:
+            filename = os.path.abspath(os.path.curdir) + path
+            try:
+                with open(filename, 'rb') as f:
+                    self.wfile.write(f.read())
+                    
+            except FileNotFoundError:
+                self.send_error(404, "File not found")
+
+
+if __name__ == "__main__":
+    port = int(sys.argv[1])
+    Handler = MyHTTPRequestHandler
+    messages = []
+    users = {}
+    
+    with open("kaoz.html", 'rb') as f:
+        pajenn_degemer = f.read()
+    
+    with socketserver.TCPServer(("", port), Handler) as httpd:
+        print("serving at port", port)
+        httpd.serve_forever()
+        
